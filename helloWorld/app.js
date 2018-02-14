3278decfbd715918854656a64ef82e369731e60d
bjoern.liebke <bjoern.liebke@bauermedia.com>
2018-02-14 14:46:50 +0100

    hello to hostname and ip adress eth0

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -6,11 +6,15 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const os = require("os");
+const hostname = os.hostname();
+const ipinterface = os.networkInterfaces();
+console.dir(ipinterface);
 // App
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello i am the webapp</h1>\n');
+  res.send('<h1>Hello i am the webapp</h1>\nand this is my hostname '+hostname+" with ip "+ipinterface.eth0[0].address);
 });
 
 app.listen(PORT, HOST);
