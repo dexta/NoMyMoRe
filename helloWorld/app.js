0d38462e899434abf2ea7c6ab09f698afc7fc10a
dexta <dexta.de@gmail.com>
2018-02-18 21:39:32 +0100

    weekend with ci/cd/docker/registry

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -9,13 +9,27 @@ const HOST = '0.0.0.0';
 const os = require("os");
 const hostname = os.hostname();
 const ipinterface = os.networkInterfaces();
-console.dir(ipinterface);
+const hostip = (ipinterface.eth0||false)? ipinterface.eth0[0].address : '0.0.0.0';
+// console.dir(ipinterface);
 // App
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello i am the webapp</h1>\nand this is my hostname '+hostname+" with ip "+ipinterface.eth0[0].address);
+  let msg = `<h1>Hello i am the webapp</h1>`;
+      msg += `<h3>and this is my hostname ${hostname}</h3>`;
+      msg += `<h2>with ipv4 address ${hostip} on eth0</h2>`;
+      msg += `some env varibles: NODE_hello ${process.env.NODE_hello} ; NODE_ENV ${process.env.NODE_ENV}`;
+  res.send(msg);
+});
+
+app.get('/test/truefalse/', (req,res) => {
+  res.status(200).json({testTrue:true,testFalse:false});
+});
+app.get('/test/env/', (req,res) => {
+  res.status(200).json(process.env);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports = app; // for testing
